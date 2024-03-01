b8440feec685580e2cb7e087dd0f2c9aa86b2412
bjrfx <phanikiran1234@gmail.com>
2024-02-29 19:51:42 -0500

    Buttons
    -Types of buttons,
      - Custom colors for buttons.
    -Typograph.
    -Usage of styled.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-
+import { Button, Typography } from "@mui/material";
+import SettingsIcon from '@mui/icons-material/Settings';
+import styled from "@emotion/styled";
 
 function App() {
+  const CrimsonButton = styled(Button) ({
+    backgroundColor: "crimson",
+    color: "white",
+    margin: 5,
+    "&:hover": {
+      backgroundColor: "#E2654B"
+    }
+  })
   return (
     <div>
-     
+    <Button variant="text">Text</Button>
+    <Button endIcon={<SettingsIcon />} size="small" color="warning" variant="contained">Settings</Button>
+    <Button variant="outlined">Outlined</Button>
+    <Typography variant="h1" component="p">
+      It uses h1 style but it's a P tag
+    </Typography>
+    <CrimsonButton>Crimson</CrimsonButton>
     </div>
   );
 }
