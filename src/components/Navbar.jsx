554d198733723bafbf89cfd86d9e383277294f37
bjrfx <57858722+bjrfx@users.noreply.github.com>
2024-03-04 16:47:56 -0500

    Updated Navbar
    - Added Search
    - Added Custom Icons on Badge
    - Added Avatar ( Profile Image )
    - Aligned Icons
    - Visible all these components on Mobile and Desktop screens.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,52 @@
 import styled from '@emotion/styled'
-import { AppBar, Toolbar, Typography } from '@mui/material'
+import { AppBar, Toolbar, Typography, Box, InputBase } from '@mui/material'
 import React from 'react'
 import SelfImprovementIcon from '@mui/icons-material/SelfImprovement';
+import Badge from '@mui/material/Badge';
+import MailIcon from '@mui/icons-material/Mail';
+import Notifications from '@mui/icons-material/Notifications';
+import Avatar from '@mui/material/Avatar';
 
 const Navbar = () => {
+  //Custom Toolbar
   const StyledToolbar = styled(Toolbar)({
     display: "flex",
     justifyContent: "space-between"
   })
+
+  // custom search bar
+  const Search = styled("div")(({theme}) => ({
+    backgroundColor: "white",
+    padding: "0 10px",
+    borderRadius: "5px",
+    width: "40%"
+  }));
+
+  //Icon
+  const Icons = styled(Box)(({theme}) => ({
+    display: "flex",
+    gap: "20px",
+    alignItems: "center"
+  }))
+  
   return (
     <AppBar position='sticky'>
       <StyledToolbar>
         <Typography variant='h6' sx={{display: {xs: "none", sm: "block"}}}>Bagavad Gita</Typography>
         <SelfImprovementIcon sx={{display: {xs: "block", sm: "none"}}} />
+        <Search><InputBase placeholder='search...'/></Search>
+      <Icons>
+      {/* Mail Badge */}
+      <Badge badgeContent={4} color="error" >
+        <MailIcon color="white" />
+      </Badge>
+      {/* Mail Badge */}
+      <Badge badgeContent={4} color="error">
+        <Notifications color="white" />
+      </Badge>
+      {/* Avatar */}
+    <Avatar alt= "Remy Sharp" sx={{width:30, height:30}}/>
+      </Icons>
       </StyledToolbar>
     </AppBar>
   )
