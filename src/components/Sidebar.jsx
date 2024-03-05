e3fab780201a4dcc5c364b29eec39ad723a1b330
bjrfx <57858722+bjrfx@users.noreply.github.com>
2024-03-04 19:02:46 -0500

    Updated Sidebar ( Menu )
    -Added some more menu items with icons.

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,8 +1,105 @@
 import React from 'react'
-import { Box } from '@mui/material'
+import { Box, List, ListItem, ListItemButton, ListItemIcon, ListItemText } from '@mui/material'
+import HomeIcon from '@mui/icons-material/Home';
+import BookIcon from '@mui/icons-material/Book';
+import Admin from '@mui/icons-material/AdminPanelSettings';
+import HelpIcon from '@mui/icons-material/Help';
+import SupportIcon from '@mui/icons-material/Support';
+import AudiotrackIcon from '@mui/icons-material/Audiotrack';
+import SettingsIcon from '@mui/icons-material/Settings';
+import Switch from '@mui/material/Switch';
+import Night from '@mui/icons-material/Brightness6';
 const Sidebar = () => {
   return (
-    <Box bgcolor="grey" flex={1} p={2} sx={{ display: {xs: "none", sm: "block"}}}>Sidebar</Box>
+    <Box  flex={1} p={2} sx={{ display: {xs: "none", sm: "block"}}}>
+      <List>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href='#home'>
+              <ListItemIcon>
+                <HomeIcon />
+              </ListItemIcon>
+              <ListItemText primary="Homepage" />
+            </ListItemButton>
+          </ListItem>
+        </List>
+
+        <List>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href='#blog'>
+              <ListItemIcon>
+                <BookIcon />
+              </ListItemIcon>
+              <ListItemText primary="Blog" />
+            </ListItemButton>
+          </ListItem>
+        </List>
+
+        <List>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href='#admin'>
+              <ListItemIcon>
+                <Admin />
+              </ListItemIcon>
+              <ListItemText primary="Admin" />
+            </ListItemButton>
+          </ListItem>
+        </List>
+
+        <List>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href='#help'>
+              <ListItemIcon>
+                <HelpIcon />
+              </ListItemIcon>
+              <ListItemText primary="Help" />
+            </ListItemButton>
+          </ListItem>
+        </List>
+
+        <List>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href='#support'>
+              <ListItemIcon>
+                <SupportIcon />
+              </ListItemIcon>
+              <ListItemText primary="Support" />
+            </ListItemButton>
+          </ListItem>
+        </List>
+
+        <List>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href='#audio'>
+              <ListItemIcon>
+                <AudiotrackIcon />
+              </ListItemIcon>
+              <ListItemText primary="Audio" />
+            </ListItemButton>
+          </ListItem>
+        </List>
+
+        <List>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href='#settings'>
+              <ListItemIcon>
+                <SettingsIcon />
+              </ListItemIcon>
+              <ListItemText primary="Settings" />
+            </ListItemButton>
+          </ListItem>
+        </List>
+
+        <List>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href='#settings'>
+              <ListItemIcon>
+              <Night />
+              </ListItemIcon>
+              <Switch />
+            </ListItemButton>
+          </ListItem>
+        </List>
+    </Box>
   )
 }
 
